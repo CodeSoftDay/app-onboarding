bf22a4e86de3b326af552270643516a9ade15897
manuelduarte077 <carlosd99m@gmail.com>
2021-01-03 15:44:04 -0600

    Revicion de Bugs :bug:

--- app/src/main/java/com/hardsoftday/apponboarding/MainActivity.kt
+++ app/src/main/java/com/hardsoftday/apponboarding/MainActivity.kt
@@ -2,12 +2,40 @@ package com.hardsoftday.apponboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.hardsoftday.apponboarding.adapter.OnBoardingViewPagerAdapter
+import com.hardsoftday.apponboarding.model.OnBoardingData
 
 class MainActivity : AppCompatActivity() {
+
+     var onBoardingViewPagerAdapter: OnBoardingViewPagerAdapter? = null
+    var tabLayout: TabLayout? = null
+    var onBoardingViewPager : ViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tab_indicator)
+
+        // Lets add data to our model class
+
+        val onBoardingData:MutableList<OnBoardingData> = ArrayList()
+        onBoardingData.add(OnBoardingData("Fresh Food", "Con las frutas más frescas de la ciudad", R.drawable.img1))
+        onBoardingData.add(OnBoardingData("Fast Delivery", "Servico delivery mas rapido y confiable", R.drawable.img2))
+        onBoardingData.add(OnBoardingData("Easy Payment", "Son dolores de cabeza al hacer los pagos ", R.drawable.img3))
+
+        setOnBoardingViewPagerAdapter(onBoardingData)
+
+    }
+
+    private fun setOnBoardingViewPagerAdapter(onBoardingData: List<OnBoardingData>) {
+
+        onBoardingViewPager = findViewById(R.id.screenPager)
+        onBoardingViewPagerAdapter = OnBoardingViewPagerAdapter(this, onBoardingData)
+        onBoardingViewPager!!.adapter = onBoardingViewPagerAdapter
+        tabLayout?.setupWithViewPager(onBoardingViewPager)
 
     }
 
